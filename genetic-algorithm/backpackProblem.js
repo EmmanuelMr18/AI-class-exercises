b33c41add1bcc1a1d8b55d5547feb525dc34de0f
EmmanuelMr18 <emm_mr@hotmail.com>
2022-06-12 16:31:55 -0500

    feat(genetic algorithm): get population size via prompt

--- genetic-algorithm/backpackProblem.js
+++ genetic-algorithm/backpackProblem.js
@@ -1,12 +1,15 @@
-
 // Función principal donde se establecen los valores
 // y se llaman a las otras funciones
-
+const prompt = require('prompt-sync')();
 function main() {
     const itemsWeight = [22, 39, 99, 63, 68, 2];
     const itemsValue = [43, 3, 45, 12, 43, 16];
     const backpackSize = 150;
-    const population = getInitialPopulation(8, itemsWeight);
+    let populationSize = 0;
+    while (populationSize <= 0) {
+        populationSize = prompt('¿De qué tamaño quieres la población?');
+    }
+    const population = getInitialPopulation(populationSize, itemsWeight);
     const populationWeights = getPopulationWeights(population, itemsWeight);
     const populationFitness = getPopulationFitness(
         population,
@@ -30,7 +33,7 @@ function getInitialPopulation(populationSize, itemsWeight) {
 
     //Mediante 2 fors va creando los vectores
     //El primer for crea el vector y el segundo
-    //for genera el numero aleatorio ya sea 0 ó 1    
+    //for genera el numero aleatorio ya sea 0 ó 1
     for (let i = 0; i < populationSize; i++) {
         const individualBuff = [];
         for (let j = 0; j < itemsWeight.length; j++) {
@@ -47,10 +50,8 @@ function getInitialPopulation(populationSize, itemsWeight) {
 //La función getPopulationWights retorna el peso total de cada individuo
 
 function getPopulationWeights(population, itemsWeight) {
-
     //El método map nos permite iterar sobre el arreglo
     const populationWeights = population.map((individual) => {
-
         //El método reduce nos permite llamar cada elemento del vector
         return individual.reduce((acc, itemBool, index) => {
             if (itemBool === 0) {
@@ -62,7 +63,6 @@ function getPopulationWeights(population, itemsWeight) {
     return populationWeights;
 }
 
-
 //La función getPopultationFitness retorna el fitness de cada individuo
 
 function getPopulationFitness(
@@ -72,7 +72,6 @@ function getPopulationFitness(
     backpackSize
 ) {
     const populationFitnessBuff = population.map((individual, index) => {
-
         //Si el peso es mayor a la capacidad se retorna a 0
         if (populationWeights[index] > backpackSize) {
             return 0;
@@ -90,7 +89,6 @@ function getPopulationFitness(
 //La función getFathers retorna los padres seleccionados
 
 function getFathers(population, populationFitness) {
-
     //Elige aleatoriamente los individuos que competiran
 
     const fathersBuff = population.map((_, index) => {
